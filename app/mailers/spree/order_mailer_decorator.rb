912d462e0d25627697c142781a8e79096f63e645
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2014-08-08 22:33:33 -0500

    Update for changes in Spree 2.4.0 beta

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,28 +1,30 @@
 Spree::OrderMailer.class_eval do
   def confirm_email(order, resend = false)
     find_order(order)
-    subject = (resend ? "[#{t(:resend).upcase}] " : '')
-    subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{order.number}"
-    if order.dibs_referral
-      mail(to: order.email, from: from_address, subject: subject, cc: order.dibs_referral.email)
+    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+    subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.confirm_email.subject')} ##{@order.number}"
+    if @order.dibs_referral
+      mail(to: @order.email, from: from_address, subject: subject, cc: @order.dibs_referral.email)
     else
-      mail(to: order.email, from: from_address, subject: subject)
+      mail(to: @order.email, from: from_address, subject: subject)
     end
   end
 
   def cancel_email(order, resend = false)
     find_order(order)
-    subject = (resend ? "[#{t(:resend).upcase}] " : '')
-    subject += "#{Spree::Config[:site_name]} #{t('order_mailer.cancel_email.subject')} ##{order.number}"
-    if order.dibs_referral
-      mail(to: order.email, from: from_address, subject: subject, cc: order.dibs_referral.email)
+    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
+    subject += "#{Spree::Store.current.name} #{Spree.t('order_mailer.cancel_email.subject')} ##{@order.number}"
+    if @order.dibs_referral
+      mail(to: @order.email, from: from_address, subject: subject, cc: @order.dibs_referral.email)
     else
-      mail(to: order.email, from: from_address, subject: subject)
+      mail(to: @order.email, from: from_address, subject: subject)
     end
   end
 
   private
   def find_order(order)
-    @order = order.is_a?(Spree::Order) ? order : Spree::Order.find(order)
+    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
   end
 end
+
+
